8deefae07b40e5a9c29f194aa165a73798b5a025
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-09-11 09:00:38 -0300

    add ordenacao

---
+++ src/componentes/Ordenacao/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function Ordenacao() {
+    return (
+        <div className="container-ordenacao">
+            <button className="botao--ativo">Recentes</button>
+            <button className="botao">Recentes</button>
+        </div>
+    )
+}
